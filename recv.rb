b158ce1b02646f9487834f20af8f240982c6cb91
Claus Lensbøl <cmol@cmol.dk>
2018-03-07 16:32:12 +0100

    More testing

--- recv.rb
+++ recv.rb
@@ -1,16 +1,17 @@
 require 'socket'
 require 'ipaddr'
-MULTICAST_ADDR = "225.4.5.6"
+MULTICAST_ADDR = "224.0.0.100"
 LOCAL_ADDR     = "172.16.0.2"
 PORT = 5000
 #ip =  IPAddr.new(MULTICAST_ADDR).hton + IPAddr.new("0.0.0.0").hton
 ip =  IPAddr.new(MULTICAST_ADDR).hton + IPAddr.new(LOCAL_ADDR).hton
 sock = UDPSocket.new
 sock.setsockopt(Socket::IPPROTO_IP, Socket::IP_ADD_MEMBERSHIP, ip)
-sock.bind(LOCAL_ADDR, PORT)
-#sock.bind(Socket::INADDR_ANY, PORT)
+sock.setsockopt(Socket::IPPROTO_IP, Socket::IP_MULTICAST_LOOP, [1].pack("i"))
+#sock.bind(LOCAL_ADDR, PORT)
+sock.bind(Socket::INADDR_ANY, PORT)
 loop do
-  msg, info = sock.recvfrom(1024)
+  msg, info = sock.recvfrom(1500)
   puts "MSG: #{msg} from #{info[2]} (#{info[3]})/#{info[1]} len #{msg.size}"
 end
 
--- send.rb
+++ send.rb
@@ -1,20 +1,21 @@
 require 'socket'
 require 'ipaddr'
 #MULTICAST_ADDR = "225.4.5.6"
-MULTICAST_ADDR = "224.0.0.22"
+MULTICAST_ADDR = "224.0.0.100"
 SOURCE_ADDR    = "172.16.0.1"
 #MULTICAST_ADDR = "10.16.160.1"
 PORT= 5000
-10.times do
+1.times do
 begin
   socket = UDPSocket.open
+  socket.setsockopt(Socket::IPPROTO_IP, Socket::IP_MULTICAST_LOOP, [1].pack("i"))
   socket.setsockopt(Socket::IPPROTO_IP, Socket::IP_MULTICAST_TTL, [1].pack('i'))
   socket.setsockopt(Socket::IPPROTO_IP, Socket::IP_MULTICAST_IF, IPAddr.new(SOURCE_ADDR).hton)
 #  socket.bind("172.16.0.1", 0)
   #socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_BINDTODEVICE, "enp0s25"+'\0')
   start = Time.now
   10000.times do
-    socket.send("MAXMULTICASTMT" * 100, 0, MULTICAST_ADDR, PORT)
+    socket.send("1" * 1472, 0, MULTICAST_ADDR, PORT)
   end
   ending = Time.now
 ensure
