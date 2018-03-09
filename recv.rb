c4af62626f02b920d2f24c0825be94fa5aafd030
Claus Lensbøl <cmol@cmol.dk>
2018-03-09 16:02:09 +0100

    Working IPv6 version

--- recv.rb
+++ recv.rb
@@ -1,7 +1,7 @@
 require 'socket'
 require 'ipaddr'
 MULTICAST_ADDR = "224.0.0.100"
-LOCAL_ADDR     = "172.16.0.2"
+LOCAL_ADDR     = "10.7.10.65"
 PORT = 5000
 #ip =  IPAddr.new(MULTICAST_ADDR).hton + IPAddr.new("0.0.0.0").hton
 ip =  IPAddr.new(MULTICAST_ADDR).hton + IPAddr.new(LOCAL_ADDR).hton
@@ -12,6 +12,7 @@ sock.setsockopt(Socket::IPPROTO_IP, Socket::IP_MULTICAST_LOOP, [1].pack("i"))
 sock.bind(Socket::INADDR_ANY, PORT)
 loop do
   msg, info = sock.recvfrom(1500)
-  puts "MSG: #{msg} from #{info[2]} (#{info[3]})/#{info[1]} len #{msg.size}"
+#  puts "MSG: #{msg} from #{info[2]} (#{info[3]})/#{info[1]} len #{msg.size}"
+  printf(".")
 end
 
