f2abe0f05659ad8ad39534edadffcc1546ba904f
Claus Lensbøl <cmol@cmol.dk>
2018-04-04 12:51:54 +0200

    Dont reference self

--- lib/mcast_perf_test/experiment.rb
+++ lib/mcast_perf_test/experiment.rb
@@ -4,13 +4,13 @@ module McastPerfTest
 
     bitrates.each do | bitrate |
       package_sizes.each do | size |
-        sender = McastPerfTest::Sender.new(
+        sender = Sender.new(
           {bitrate: bitrate,
            pkg_length: size,
            wifi: wifi,
            ethernet: ethernet})
         sender.run
-        collector = McastPerfTest::Collector.new(name + date_string, clients)
+        collector = Collector.new(name + date_string, clients)
         collector.run
 
         # Sleep to let possible congestion pass
